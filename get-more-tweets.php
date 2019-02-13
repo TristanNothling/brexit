b98b263bd29e0f30b015d2f1ff502b5baa0f7dd6
Tristan Nothling <tristannothling@live.com>
2019-02-13 21:04:36 +0000

    writing to text file db

--- get-more-tweets.php
+++ get-more-tweets.php
@@ -1,5 +1,4 @@
 <?php 
-
 $consumer_key = "yReEmIJrCRcN3joTRyBEoGXAk";
 $consumer_secret_key = "puh97Sb0pkHhzSLrwQw5RzPfpU5jesd5v5fib9vRF7GwxOhiSa";
 
@@ -11,10 +10,8 @@ curl_setopt($ch, CURLOPT_URL, "https://api.twitter.com/oauth2/token");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
 curl_setopt($ch, CURLOPT_POST,           1 );
 curl_setopt($ch, CURLOPT_POSTFIELDS,     "grant_type=client_credentials" ); 
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('grant_type: client_credentials','Content-Type: application/x-www-form-urlencoded','Authorization: Basic '. $bearer_tc)); 
-
-/*Retrieve access token from Twitter authorisation endpoint*/
-                                                             
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded','Authorization: Basic '. $bearer_tc)); 
+                                                            
 $result = curl_exec($ch);
 
 $json_dict = json_decode($result, true);
@@ -29,12 +26,21 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '. $access_token)); 
 
 $result = curl_exec($ch);
-
 $just_tweets = json_decode($result,true);
 
+$outputFileName = "tweets.txt"; /*quick way to make a db*/
+$logFile = fopen("/kunden/homepages/29/d697604814/htdocs/Bid4MyJob/trexit/".$outputFileName, "r") or die("Unable to open the txt file.");
+$tweet_buffer_string = "";
+
 foreach ($just_tweets['statuses'] as $oneTweet)
-{
-	echo '<p>'.$oneTweet['text'].'</p>';
-}
+	{
+    echo $oneTweet['id_str'] . ' ~ ' . $oneTweet['text'] . ' ~ ' . $oneTweet['created_at'] PHP_EOL;
+    //echo '<p>'.$oneTweet['text'].'</p>';
+	}
+
+fclose($logFile);
+$logFile = fopen("/kunden/homepages/29/d697604814/htdocs/Bid4MyJob/trexit/".$outputFileName,"a") or die("Unable to open the txt file.");
+fwrite($logFile,$tweet_buffer_string);
+fclose($logFile);
 
 ?>
--- index.php
+++ index.php
@@ -1,51 +1,29 @@
-<?php 
-
-$consumer_key = "yReEmIJrCRcN3joTRyBEoGXAk";
-$consumer_secret_key = "puh97Sb0pkHhzSLrwQw5RzPfpU5jesd5v5fib9vRF7GwxOhiSa";
-
-$twitter_ = $consumer_key.":".$consumer_secret_key;
-$bearer_tc = base64_encode($twitter_);
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://api.twitter.com/oauth2/token");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
-curl_setopt($ch, CURLOPT_POST,           1 );
-curl_setopt($ch, CURLOPT_POSTFIELDS,     "grant_type=client_credentials" ); 
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('grant_type: client_credentials','Content-Type: application/x-www-form-urlencoded','Authorization: Basic '. $bearer_tc)); 
-
-/*Retrieve access token from Twitter authorisation endpoint*/
-                                                             
-$result = curl_exec($ch);
-
-$json_dict = json_decode($result, true);
-$access_token = ($json_dict['access_token']);
-
-$ch = curl_init();
-
-$query_string = '?f=tweets&q=brexit&count=50';
-
-curl_setopt($ch, CURLOPT_URL, "https://api.twitter.com/1.1/search/tweets.json" . $query_string);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer '. $access_token)); 
-
-$result = curl_exec($ch);
-
-$just_tweets = json_decode($result,true);
-
-?>
-
 <!DOCTYPE html>
 <html>
 <title></title>
 <body>
-<?php 
+<div class="tweet-list" style="width:66%;height:100%;padding:1rem;">
+</div>
+</body>
 
-foreach ($just_tweets['statuses'] as $oneTweet)
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous">
+</script>
+  
+<script>
+function get_more_tweets()
 {
-	echo $oneTweet['text'].'<br>';
+	var request = $.get({
+		url: "http://bid4myjob.co.uk/get-more-tweets.php",
+		method: "GET"
+		});
+	request.done(function(tweets)
+		{        
+        console.log(tweets);
+		$('.tweet-list').append(tweets);
+		});
 }
 
-?>
+get_more_tweets();
 
-</body>
+</script>
 </html>
